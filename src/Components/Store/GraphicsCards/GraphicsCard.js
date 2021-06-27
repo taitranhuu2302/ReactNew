8e9fdebafe0cf90ccd0bc1f7f4929dab1eda796f
taitranhuu1908 <emlacuaanh1908@gmail.com>
2021-06-27 20:37:33 +0700

    123

--- src/Components/Store/GraphicsCards/GraphicsCard.js
+++ src/Components/Store/GraphicsCards/GraphicsCard.js
@@ -1,82 +1,95 @@
-import React, { Component } from 'react';
-import Slider from 'react-slick';
+import React, { Component } from "react";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./styles.scss";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class GraphicsCard extends Component {
-    render() {
-        const settings = {
-            dots: false,
-            infinite: true,
-            speed: 500,
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
             slidesToShow: 3,
             slidesToScroll: 3,
-            responsive: [
-                {
-                    breakpoint: 1025,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                    },
-                },
-                {
-                    breakpoint: 769,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        initialSlide: 2,
-                    },
-                },
-                {
-                    breakpoint: 481,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                    },
-                },
-            ],
-        };
-        return (
-            <div className="container-fluid" id="graphics-card">
-                <div className="container-fluid title">
-                    <div className="container">
-                        <h2 className="text-center">GRAPHICS CARDS</h2>
-                        <Slider {...settings} className="slides">
-                            <div className="slide-item">
-                                <Link to="/">
-                                    <div className="img">
-                                        <img src="https://www.aorus.com/assets/img/graphics-cards-aorus.58760d9d.png" className="w-100 img-fluid" alt="" />
-                                    </div>
-                                    <div className="slide-title">
-                                        AORUS
-                                    </div>
-                                </Link>
-                            </div>
-                            <div className="slide-item">
-                                <div className="img">
-                                    <img src="https://www.aorus.com/assets/img/graphics-cards-aorus.58760d9d.png" className="w-100 img-fluid" alt="" />
-                                </div>
-                                <div className="slide-title">
-                                    AORUS
-                                </div>
-                            </div>
-                            <div className="slide-item">
-                                <div className="img">
-                                    <img src="https://www.aorus.com/assets/img/graphics-cards-aorus.58760d9d.png" className="w-100 img-fluid" alt="" />
-                                </div>
-                                <div className="slide-title">
-                                    AORUS
-                                </div>
-                            </div>
-                        </Slider>
-                    </div>
-                </div>
+            infinite: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1,
+          },
+        },
+        {
+          breakpoint: 481,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    };
+    return (
+      <div className="container-fluid" id="graphics-card">
+        <div className="container-fluid title">
+          <div className="container">
+            <h2 className="text-center">GRAPHICS CARDS</h2>
+            <Slider {...settings} className="slides">
+              {this.showGraphicCardList(listCard)}
+            </Slider>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  showGraphicCardList = (arr) => {
+    var result = null;
+
+    result = arr.map((item, index) => {
+      return (
+        <div key={index} className="slide-item">
+          <div className="text-decoration-none bgr" to="/">
+            <div className="img">
+              <Link to={item.to}>
+                <img src={item.image} className="w-100 img-fluid" alt="" />
+              </Link>
             </div>
-        );
-    }
+            <div className="slide-title">{item.name}</div>
+          </div>
+        </div>
+      );
+    });
+
+    return result;
+  };
 }
+const listCard = [
+  {
+    to: "/GraphicsCard",
+    name: "AORUS",
+    image: "https://www.aorus.com/assets/img/graphics-cards-aorus.58760d9d.png",
+  },
+  {
+    to: "/GraphicsCard",
+    name: "NVIDIA SERIES",
+    image:
+      "https://www.aorus.com/assets/img/graphics-cards-nvidia.7025d4f5.png",
+  },
+  {
+    to: "/GraphicsCard",
+    name: "AMD SERIES",
+    image: "https://www.aorus.com/assets/img/graphics-cards-amd.4c5818ab.png",
+  },
+];
 
 export default GraphicsCard;
--- src/Pages/StorePage/GraphicsCards.js
+++ src/Pages/StorePage/GraphicsCards.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import GraphicsCard from "./../../Components/Store/GraphicsCards/GraphicsCard";
-import Header from './../../Components/LoginPage/HeaderLogin/Header/Header'
+import Header from "./../../Components/LoginPage/HeaderLogin/Header/Header";
 import Breadcumb from "../../Components/LoginPage/ContentLogin/Breadcumb/Breadcumb";
-import Footer from "../../Components/HomePage/Footer/Footer";
-import Banner from './../../Components/HomePage/Banner/Banner'
+// import Footer from "../../Components/HomePage/Footer/Footer";
+import Banner from "./../../Components/HomePage/Banner/Banner";
 
 class GraphicsCards extends Component {
   render() {
-    return <>
-      <Header />
-      <Breadcumb to="/" toBack="Home" current="Graphics Cards"/>
-      <Banner/>
-      <GraphicsCard />
-      {/* <Footer /> */}
-    </>;
+    return (
+      <>
+        <Header />
+        <Breadcumb to="/" toBack="Home" current="Graphics Cards" />
+        <Banner />
+        <GraphicsCard />
+        {/* <Footer /> */}
+      </>
+    );
   }
 }
 
