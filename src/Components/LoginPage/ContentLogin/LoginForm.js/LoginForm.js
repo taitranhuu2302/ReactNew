cd334b610a754896b6bea5f4a18739b7cec2a0e7
Tran Huu Tai <76210961+taitranhuu1908@users.noreply.github.com>
2022-01-16 22:06:36 +0700

    update

--- src/Components/LoginPage/ContentLogin/LoginForm.js/LoginForm.js
+++ src/Components/LoginPage/ContentLogin/LoginForm.js/LoginForm.js
@@ -5,63 +5,63 @@ import { connect } from "react-redux";
 import * as action from "./../../../../Actions/index";
 
 class LoginForm extends Component {
-  componentDidMount() {
-    this.props.onGetUsers();
-  }
+    componentDidMount() {
+        this.props.onGetUsers();
+    }
 
-  onChangeLogin = () => {
-    this.props.onChangeLogin();
-  };
-  onLogged = (data) => {
-    this.props.onLogged(data);
-  };
+    onChangeLogin = () => {
+        this.props.onChangeLogin();
+    };
+    onLogged = (data) => {
+        this.props.onLogged(data);
+    };
 
-  render() {
-    var { changeLogin, match, users } = this.props;
-    var url = match.url;
-    var slug = changeLogin ? "register" : "login";
-    return (
-      <div className="container-fluid px-0 py-5" id="wapper-login">
-        <div className="content-box">
-          <div className="container-fluid">
-            <div className="row">
-              <InputLogin
-                onChangeLogin={this.onChangeLogin}
-                url={url}
-                slug={slug}
-                users={users}
-                logged={this.props.logged}
-                onLogged={this.onLogged}
-                history={this.props.history}
-              />
+    render() {
+        var { changeLogin, match, users } = this.props;
+        var url = match.url;
+        var slug = changeLogin ? "register" : "login";
+        return (
+            <div className="container-fluid px-0 py-5" id="wapper-login">
+                <div className="content-box">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <InputLogin
+                                onChangeLogin={this.onChangeLogin}
+                                url={url}
+                                slug={slug}
+                                users={users}
+                                logged={this.props.logged}
+                                onLogged={this.onLogged}
+                                history={this.props.history}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    changeLogin: state.changeLogin,
-    logged: state.logged,
-    users: state.users,
-  };
+    return {
+        changeLogin: state.changeLogin,
+        logged: state.logged,
+        users: state.users,
+    };
 };
 
 const mapDispatchToProps = (dispatch, props) => {
-  return {
-    onChangeLogin: () => {
-      dispatch(action.onChangeLogin());
-    },
-    onLogged: (username) => {
-      dispatch(action.acOnLogged(username));
-    },
-    onGetUsers: () => {
-      dispatch(action.acGetUsersRequest());
-    },
-  };
+    return {
+        onChangeLogin: () => {
+            dispatch(action.onChangeLogin());
+        },
+        onLogged: (username) => {
+            dispatch(action.acOnLogged(username));
+        },
+        onGetUsers: () => {
+            dispatch(action.acGetUsersRequest());
+        },
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
